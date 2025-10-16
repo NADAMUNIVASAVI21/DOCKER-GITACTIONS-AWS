5c7b7102fbf54588ca49c4fbe556b091341b77ce
NADAMUNI VASAVI <2300032734@kluniversity.in>
2025-10-16 09:27:39 +0530

    port change

--- CAKEAPI-SPRINGBOOT/src/main/java/com/cakes/controller/CakeController.java
+++ CAKEAPI-SPRINGBOOT/src/main/java/com/cakes/controller/CakeController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:5173") // React dev port
+@CrossOrigin(origins = "*") // React dev port
 public class CakeController {
 
     @Autowired
